4b3b29b7a0131da2cb92a9b171a9696a2a219b86
Richard Cook <richard@cookra.co.uk>
2017-08-11 20:04:56 +0100

    Empty RAB

--- App.js
+++ App.js
@@ -2,8 +2,5 @@ Ext.define('CustomApp', {
     extend: 'Rally.app.App',
     componentCls: 'app',
     launch: function() {
-        //Write app code here
-asdfadfasfsdf
-        //API Docs: https://help.rallydev.com/apps/2.1/doc/
     }
 });
